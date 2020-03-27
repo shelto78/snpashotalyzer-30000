d22c447b76b01a197dfc3a4e5895735e7f1083d4
D S <david.shelton.78@gmail.com>
2020-03-28 02:07:50 +1100

    Setup.py to enable building

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="snapshotalyzer-30000",
+    version='0.1',
+    author="David Shelton",
+    author_email="david.shelton.78@gmail.com",
+    description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots",
+    license="GPLv3+",
+    packages=['shotty'],
+    url="https://github.com/shelto78/snpashotalyzer-30000",
+    install_requires=['click','boto3'],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+        ''',
+
+)
